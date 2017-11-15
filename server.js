9f8d073c7aaf4850f2c9e92bb62a7861316ddfe8
sophiasheref <sophia.sheref@gmail.com>
2017-11-15 11:21:35 -0500

    done

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var PORTONE = 7000;
+var PORTTWO = 7500;
+function handleRequestOne(request, response) {
+  response.end("Route one");
+}
+function handleRequestTwo(request, response) {
+  response.end("Route two");
+}
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
+
+serverOne.listen(PORTONE, function() {
+  console.log("Server listening on: http://localhost:", PORTONE);
+});
+
+serverTwo.listen(PORTTWO, function() {
+  console.log("Server listening on: http://localhost:", PORTTWO);
+});
